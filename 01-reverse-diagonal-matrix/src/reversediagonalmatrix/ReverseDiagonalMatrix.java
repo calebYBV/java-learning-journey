1d0285362e73b8f446b8650ca837a3d2f206ac7c
CalebYBV <caleb.yatsarbv@gmail.com>
2026-07-07 23:00:21 -0500

    Implement reverse diagonal matrix population
    Initialize a 9x9 matrix and populate the reverse diagonal with values.

--- 01-reverse-diagonal-matrix/src/reversediagonalmatrix/ReverseDiagonalMatrix.java
+++ 01-reverse-diagonal-matrix/src/reversediagonalmatrix/ReverseDiagonalMatrix.java
@@ -1,1 +1,26 @@
+package reversediagonalmatrix;
 
+public class ReverseDiagonalMatrix {
+
+    public static void main(String[] args) {
+        int row, column;
+        int[][] matriz = new int[9][9];
+
+        for (row = 0; row < matriz.length; row = row + 1) {
+            for (column = 0; column < matriz[row].length; column = column + 1) {
+
+                if (row + column == 8) {
+                    matriz[row][column] = row + 1;
+                }
+            }
+        }
+
+        for (row = 0; row < matriz.length; row = row + 1) {
+            for (column = 0; column < matriz[row].length; column = column + 1) {
+                System.out.print(matriz[row][column] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
