57de9a2c9dfa19827d1dd4b338ef13e4ec720ee5
CalebYBV <caleb.yatsarbv@gmail.com>
2026-07-07 22:34:40 -0500

    Implement ReverseDiagonalMatrix functionality
    Initialize a 9x9 matrix and populate the reverse diagonal with values.

--- 01-reverse-diagonal-matrix/src/reversediagonalmatrix/ReverseDiagonalMatrix.java
+++ 01-reverse-diagonal-matrix/src/reversediagonalmatrix/ReverseDiagonalMatrix.java
@@ -1,1 +1,26 @@
+package reversediagonalmatrix;
 
+public class ReverseDiagonalMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] matriz = new int[9][9];
+
+        for (int row = 0; row < matriz.length; row++) {
+            for (int column = 0; column < matriz[row].length; column++) {
+
+                if (row + column == 8) {
+                    matriz[row][column] = row + 1;
+                }
+            }
+        }
+
+        for (int row = 0; row < matriz.length; row++) {
+            for (int column = 0; column < matriz[row].length; column++) {
+                System.out.print(matriz[row][column] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
